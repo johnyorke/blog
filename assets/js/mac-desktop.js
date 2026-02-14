39498ca79d851bb9d28a2a2e67a8d6e6efc9030c
John Yorke <yorkej@mac.com>
2026-02-14 10:49:25 +0000

    Update footer

--- assets/js/mac-desktop.js
+++ assets/js/mac-desktop.js
@@ -32,4 +32,54 @@
 
   updateClock();
   window.setInterval(updateClock, 30000);
+
+  function toTitleCase(value) {
+    return value
+      .split(/[-_ ]+/)
+      .filter(Boolean)
+      .map(function (part) {
+        return part.charAt(0).toUpperCase() + part.slice(1).toLowerCase();
+      })
+      .join(" ");
+  }
+
+  function detectLanguage(container) {
+    var code = container.querySelector("code");
+    if (!code) {
+      return "";
+    }
+
+    var dataLang = code.getAttribute("data-lang");
+    if (dataLang) {
+      return toTitleCase(dataLang);
+    }
+
+    var classes = (code.className || "").split(/\s+/);
+    for (var i = 0; i < classes.length; i += 1) {
+      if (classes[i].indexOf("language-") === 0) {
+        return toTitleCase(classes[i].replace("language-", ""));
+      }
+    }
+
+    var containerClasses = (container.className || "").split(/\s+/);
+    for (var j = 0; j < containerClasses.length; j += 1) {
+      if (containerClasses[j].indexOf("language-") === 0) {
+        return toTitleCase(containerClasses[j].replace("language-", ""));
+      }
+    }
+
+    return "";
+  }
+
+  function setCodeLabels() {
+    var blocks = document.querySelectorAll(".prose div.highlighter-rouge, .prose figure.highlight");
+    blocks.forEach(function (block) {
+      var language = detectLanguage(block);
+      if (language) {
+        block.setAttribute("data-code-label", language);
+      }
+    });
+  }
+
+  setCodeLabels();
 })();
